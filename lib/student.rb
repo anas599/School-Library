3fdaa25adec6684cdc944037418f08733e22c121
anas599 <anas599@yahoo.com>
2023-04-12 00:13:12 +0300

    Updates required changes

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,17 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :name, :classroom
+  attr_accessor :name
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
     classroom.students << self
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
