dabcce5574805da236252ebc83c4f74e26067511
anas599 <anas599@yahoo.com>
2023-04-13 11:29:46 +0300

    update person class

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,17 @@
 require_relative 'nameable'
 require_relative 'rental'
 require_relative 'book'
+require_relative 'classroom'
 
 class Person < Nameable
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name)
+  def initialize(age, _parent_permission, name = 'Unknown')
+    super()
     @id = Random.rand(1..9999)
+    @name = name
     @age = age
-    @parent_permission = parent_permission
+    @parent_permission = true
     @rentals = []
   end
-
   attr_reader :id
   attr_accessor :name, :age, :rentals
 
@@ -38,5 +39,28 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
+  end
+
+  class BaseDecorator < Nameable
+    def initialize(nameable)
+      super()
+      @nameable = nameable
+    end
+
+    def correct_name
+      @nameable.correct_name
+    end
+  end
+
+  class CapitalizeDecorator < BaseDecorator
+    def correct_name
+      @nameable.correct_name.capitalize
+    end
+  end
+
+  class TrimmerDecorator < BaseDecorator
+    def correct_name
+      @nameable.correct_name.slice(0, 10)
+    end
   end
 end
