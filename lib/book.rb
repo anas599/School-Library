8f513b333f69ebd9cf22b82a8d03c024648d7b18
anas599 <anas599@yahoo.com>
2023-04-18 12:51:22 +0300

    remove unused method

--- lib/book.rb
+++ lib/book.rb
@@ -15,14 +15,4 @@ class Book
     self.available = false
     rental
   end
-
-  def return_book(person, date)
-    rental = @rentals.find { |r| r.person == person && r.return_date.nil? }
-    if rental
-      rental.return_date = date
-      self.available = true
-    else
-      puts 'Error: Book is not currently rented by this person.'
-    end
-  end
 end
