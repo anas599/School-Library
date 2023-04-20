7a8843048dc948e177e0734ae00c60f12d83e047
Vanessa O <vaneolipa@gmail.com>
2023-04-20 13:05:51 -0500

    Add unit tests for teacher class

--- lib/__tests__/teacher_spec.rb
+++ lib/__tests__/teacher_spec.rb
@@ -1,12 +1,29 @@
-require 'rspec'
+require_relative '../book'
+require_relative '../classroom'
+require_relative '../rental'
+require_relative '../person'
+require_relative '../student'
 require_relative '../teacher'
+require_relative '../nameable'
 
 describe Teacher do
-  let(:teacher) { Teacher.new(30, 'Math', 'John') }
+  before(:each) do 
+    @teacher = Teacher.new(32, 'Mathematics', 'George', parent_permission: true)
+  end
 
-  describe '#new' do
-    it 'returns a Teacher object' do
-      expect(teacher).to be_an_instance_of(Teacher)
+  context 'Testing a Teacher class' do
+    it 'should return the correct age name' do
+      expect(@teacher.name).to eql 'George'
+    end
+
+    it 'should return the correct age' do
+      expect(@teacher.age).to eql 32
     end
   end
+
+    context 'Testing the can_use_services? method' do
+      it 'should always retuen true' do
+        expect(@teacher.can_use_services?).to be true
+      end
+    end
 end
